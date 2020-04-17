05ed4a4b767aafedacd46b3c351be1d06f75fd1c
Chessare44 <57406445+Chessare44@users.noreply.github.com>
2020-04-17 17:11:09 -0500

    xd

--- src/conection/connectionMongo.js
+++ src/conection/connectionMongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Chess:ajedrez94@cesarc-o6v4h.mongodb.net/baseSuple?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Chess:ajedrez94@cesarc-o6v4h.mongodb.net/BaseSuple?retryWrites=true&w=majority')
     .then(db=>{console.log('conect to mongo')})
     .catch(err=>{console.log('error al conectar')});
 
